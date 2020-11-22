dd8a341d1628001d28bf0db9acf34ee32c47558e
Leonid Korsakov <KorsakovLA@mail.ru>
2020-11-22 10:55:24 +0300

    _

--- scripts/createStartPage.js
+++
@@ -1,8 +0,0 @@
-function createStartPage(content) {
-	const enterButton = document.createElement('button')
-	enterButton.textContent = 'Get started';
-	enterButton.classList.add('enter-button');
-	content.appendChild(enterButton);
-}
-
-export default createStartPage;
--- scripts/main.js
+++
@@ -1,5 +0,0 @@
-import createStartPage from './createStartPage.js';
-
-const content = document.querySelector('.content');
-
-createStartPage(content);
